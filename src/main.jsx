7f460e4a48ea191a3f5cae22e362bbf541469dd4
Taiyob <oli1412001@gmail.com>
2023-12-24 09:01:32 +0600

    create environment setup and file setup

---
+++ src/main.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import { RouterProvider } from "react-router-dom";
+import { Toaster } from "react-hot-toast";
+import Router from "./router/Router.jsx";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+const queryClient = new QueryClient();
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <QueryClientProvider client={queryClient}>
+      <RouterProvider router={Router} />
+      <Toaster></Toaster>
+    </QueryClientProvider>
+  </React.StrictMode>
+);
---
+++ src/router/Router.jsx
@@ -0,0 +1,10 @@
+import { createBrowserRouter } from "react-router-dom";
+
+const Router = createBrowserRouter([
+  {
+    path: "/",
+    element: <h1 className="text-black">Hellow</h1>,
+  },
+]);
+
+export default Router;
